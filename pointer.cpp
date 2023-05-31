3544766e62cc6ebd805c952961be70c45d2e401b
ArafaAkter <117437951+ArafaAkter@users.noreply.github.com>
2023-05-31 20:08:49 +0600

    Add files via upload

---
+++ pointer.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b;
+   int sum=0;
+    int sub=0;
+    cin>>a>>b;
+    sum=a+b;
+    sub=abs(a-b);
+    cout<<sum<<endl<<sub;
+
+
+
+    return 0;
+}
---
+++ prepare.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i;
+    long long int l;
+    char c;
+    float f;
+    double d;
+    scanf("%d %lld %c %f %lf",&i,&l,&c,&f,&d);
+    printf("%d\n%lld\n%c\n%.3f\n%.9lf\n",i,l,c,f,d);
+
+
+    return 0;
+}
