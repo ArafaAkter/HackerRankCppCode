34d6eeec984f2dcf313149608752fa51b41b39c1
ArafaAkter <117437951+ArafaAkter@users.noreply.github.com>
2024-07-25 11:15:53 +0600

    Add files via upload

---
+++ structs.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#include<string>
+using namespace std;
+
+struct student{
+    int age;
+    string first_name;
+    string last_name;
+    int standard;
+
+
+};
+
+int main(){
+    student st;
+    cin>>st.age>>st.first_name>>st.last_name>>st.standard;
+    cout<<st.age<<" "<<st.first_name<<" "<<st.last_name<<" "<<st.standard;
+
+
+    return 0;
+}
